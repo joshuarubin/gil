50d9387968ebecdd256327dc5f76f31849e7e4cf
Joshua Rubin <jrubin@zvelo.com>
2014-05-02 16:19:22 -0600

    migrate binary search test to goconvey
    Signed-off-by: Joshua Rubin <jrubin@zvelo.com>

--- algo/search/binary_test.go
+++ algo/search/binary_test.go
@@ -1,9 +1,11 @@
 package search
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/joshuarubin/gil"
+	. "github.com/smartystreets/goconvey/convey"
 )
 
 type Result struct {
@@ -14,38 +16,39 @@ type Result struct {
 func TestBinarySearch(t *testing.T) {
 	list := gil.CopyToIntSlice([]int{1, 3, 5, 6, 7, 9})
 	expect := map[gil.Int]Result{
-		0:  {0, gil.RangeError{gil.Int(0)}},
+		0:  {0, gil.RangeError{Interface: gil.Int(0)}},
 		1:  {0, nil},
-		2:  {0, gil.NotFoundError{gil.Int(2)}},
+		2:  {0, gil.NotFoundError{Interface: gil.Int(2)}},
 		3:  {1, nil},
 		5:  {2, nil},
-		8:  {0, gil.NotFoundError{gil.Int(2)}},
+		8:  {0, gil.NotFoundError{Interface: gil.Int(2)}},
 		9:  {5, nil},
-		10: {0, gil.RangeError{gil.Int(0)}},
+		10: {0, gil.RangeError{Interface: gil.Int(0)}},
 	}
 
-	for val, res := range expect {
-		pos, err := Binary(list, val)
-		if res.Pos != pos {
-			t.Errorf("%d != %d", res.Pos, pos)
-		}
+	Convey("Given this list", t, func() {
+		for v, r := range expect {
+			val, res := v, r // make variables local to this loop only
+			pos, err := Binary(list, val)
 
-		if res.Err == nil {
-			if err != nil {
-				t.Errorf("Unexpected error, not nil (%T), for value %d (expected %d)", err, val, res.Pos)
+			if res.Err == nil {
+				Convey(fmt.Sprintf("The position of %d should be %d", val, res.Pos), func() {
+					So(res.Pos, ShouldEqual, pos)
+
+					Convey(fmt.Sprintf("No error should be returned when searching for %d", val), func() {
+						So(err, ShouldBeNil)
+					})
+				})
+				continue
 			}
-		} else {
-			if err == nil {
-				t.Errorf("Expected error, but didn't find one.")
-			} else if _, ok := res.Err.(gil.RangeError); !ok {
-				if _, ok := err.(gil.RangeError); ok {
-					t.Errorf("Unexpected error, not RangeError")
-				}
-			} else if _, ok := res.Err.(gil.NotFoundError); !ok {
-				if _, ok := err.(gil.NotFoundError); ok {
-					t.Errorf("Unexpected error, not NotFound")
-				}
-			}
+
+			Convey(fmt.Sprintf("%d should not be found", val), func() {
+				So(err, ShouldNotBeNil)
+
+				Convey(fmt.Sprintf("Error Type %T should be returned when searching for %d", res.Err, val), func() {
+					So(err, ShouldHaveSameTypeAs, res.Err)
+				})
+			})
 		}
-	}
+	})
 }
--- containers/linkedlist_test.go
+++ containers/linkedlist_test.go
@@ -7,11 +7,11 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 )
 
-func TestLinkedListReverse(t *testing.T) {
-	Convey("Linked list should be reversed", t, func() {
-		list := &LinkedList{&LinkedListNode{}}
+func TestLinkedList(t *testing.T) {
+	const NUM = 10
 
-		const NUM = 10
+	genList := func() *LinkedList {
+		list := &LinkedList{&LinkedListNode{}}
 
 		list.Walk(func(node *LinkedListNode, i int) bool {
 			node.Value = gil.Int(i)
@@ -24,8 +24,17 @@ func TestLinkedListReverse(t *testing.T) {
 			return false
 		})
 
-		list.Reverse()
+		return list
+	}
 
+	Convey("Linked list should print the correct string", t, func() {
+		list := genList()
+		So(list.String(), ShouldEqual, "0 1 2 3 4 5 6 7 8 9 10")
+	})
+
+	Convey("Linked list should be reversed", t, func() {
+		list := genList()
+		list.Reverse()
 		list.Walk(func(node *LinkedListNode, i int) bool {
 			So(node.Value, ShouldEqual, NUM-i)
 			return true
